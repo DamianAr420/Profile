c4639d023b147d36182c295aa88ed6cddd20ed5a
DamianAr420 <damianarg2115@gmail.com>
2024-10-19 19:19:24 +0200

    update

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import "./main.css";
 
 export default function main() {
+  const skills = [
+    "sql", "css", "rwd", "html", "figma", "node.js", "mongoDB", "react.js", "javascript", "express.js", "tailwindcss", "microsoft-360"
+  ]
   return (
     <div>
-      XD
+      <div className='header'>
+        <div className="img"><img src="public\me.jpg" alt="error" /></div>
+        <div className="name">
+          Hi, my name is Damian
+          <div className="desc">
+            I'm a beginner Front-end Developer. This website is about me and I'll try describe myself
+          </div>
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ul>
+          {skills.map((skill, index) => {
+            return <li key={index}>{skill}</li>
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
