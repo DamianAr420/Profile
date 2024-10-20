752b27e712b76e8dd10f1a1f31a3de6243b3c926
DamianAr420 <damianarg2115@gmail.com>
2024-10-20 18:15:37 +0200

    update

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -3,10 +3,11 @@ import "./main.css";
 
 export default function main() {
   const skills = [
-    "sql", "css", "rwd", "html", "figma", "node.js", "mongoDB", "react.js", "javascript", "express.js", "tailwindcss", "microsoft-360"
+    "sql", "css", "rwd", "html", "figma", "react.js", "node.js", "express.js", "javascript", "mongoDB", "tailwindcss", "microsoft-360"
   ]
   return (
     <div className='mainBody'>
+
       <div className='header'>
         <div className="img"><img src="public\me.jpg" alt="error" /></div>
         <div className="name">
@@ -16,14 +17,26 @@ export default function main() {
           </div>
         </div>
       </div>
-      <div className="skills">
-        <h1>Skills</h1>
-        <ul>
-          {skills.map((skill, index) => {
-            return <li key={index}>{skill}</li>
-          })}
-        </ul>
+
+      <div className='sectionOne'>
+        <div className="skills">
+          <h1>Skills</h1>
+          <ul>
+            {skills.map((skill, index) => {
+              return <li key={index}>{skill}</li>
+            })}
+          </ul>
+        </div>
+        
+        <div className="aboutMe">
+          I'm an IT student in business, specializing in front-end developer.
+          Creating websites is my hobby that I enjoy. I spend a lot of my free time learning about it.
+          I'd like to become a professional in this field in the future.
+          My second hobby is motorcycles, which are my favorite vehicle. 
+          I love traveling and visiting different places by motorcycle.
+        </div>
       </div>
+
     </div>
   )
 }
