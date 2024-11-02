6bf32b4aebed03f46875794c37b14dfdf996b10c
DamianAr420 <damianarg2115@gmail.com>
2024-11-02 18:26:26 +0100

    darkmode, navbar

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 import "./main.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
-import LoadingPage from './loadingPage/loadingPage';
+import LoadingPage from '../loadingPage/loadingPage';
 import img from "../../me.jpg";
+import { Link, Element } from 'react-scroll';
 
 export default function Main() {
   const skills = [
     "sql", "css", "rwd", "html", "figma", "react.js", "node.js", "express.js", "javascript", "mongoDB", "tailwindcss", "microsoft-360"
   ]
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [darkmode, setDarkMode] = useState(true);
 
   useEffect(() => {
     AOS.init({
@@ -20,6 +22,23 @@ export default function Main() {
     }, 5000);
   }, []);
 
+  const darkmodeSwitch = () => {
+    const dmE = document.getElementById("dmElement").style;
+    if(darkmode === true) {
+      dmE.marginLeft = "40px";
+      document.documentElement.style.setProperty('--bcg', '#fff');
+      document.documentElement.style.setProperty('--txt', '#111');
+      document.documentElement.style.setProperty('--nav', '#111');
+      setDarkMode(false)
+    } else {
+      dmE.marginLeft = "0px";
+      document.documentElement.style.setProperty('--bcg', '#111')
+      document.documentElement.style.setProperty('--txt', '#fff')
+      document.documentElement.style.setProperty('--nav', '#fff');
+      setDarkMode(true)
+    }
+  }
+
   return (
     <div>
       {isLoading ? <LoadingPage /> :
@@ -27,12 +46,16 @@ export default function Main() {
 
         <nav className="navbar">
           <ul>
-            <li><a href="#skills">Skills</a></li>
-            <li><a href="#aboutMe">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><Link className='link' to="skills" smooth={true} duration={500} offset={-150}>Skills</Link></li>
+            <li><Link className='link' to="aboutMe" smooth={true} duration={500} offset={-150}>About</Link></li>
+            <li><Link className='link' to="projects" smooth={true} duration={500} offset={-150}>Projects</Link></li>
+            <li><Link className='link' to="contact" smooth={true} duration={500} offset={-150}>Contact</Link></li>
           </ul>
         </nav>
+
+        <div onClick={darkmodeSwitch} className='darkmode'>
+          <div id='dmElement' className='dmElement'></div>
+        </div>
 
         <div className='header'>
           <div className="img"><img src={img} alt="MyPhoto" /></div>
@@ -45,26 +68,26 @@ export default function Main() {
         </div>
 
         <div className='sectionOne'>
-          <div id="skills" className="skills">
+          <Element name="skills" className="skills">
             <h1>Skills</h1>
             <ul>
               {skills.map((skill, index) => {
                 return <li key={index}>{skill}</li>
               })}
             </ul>
-          </div>
+          </Element>
 
-          <div id="aboutMe" className="aboutMe">
+          <Element name="aboutMe" className="aboutMe">
             I'm an IT student in business, specializing in front-end developer.
             Creating websites is my hobby that I enjoy. I spend a lot of my free time learning about it.
             I'd like to become a professional in this field in the future.
             My second hobby is motorcycles, which are my favorite vehicle. 
             I love traveling and visiting different places by motorcycle.
-          </div>
+          </Element>
         </div>
 
         <div className="sectionTwo">
-          <div id="projects" className="projects" data-aos="zoom-in">
+          <Element name="projects" className="projects" data-aos="zoom-in">
             <h1>My Projects</h1>
             <div className="project-list">
               <div className="project-item" data-aos="fade-up">
@@ -80,18 +103,18 @@ export default function Main() {
                 <a href="https://github.com/DamianAr420/Notepad" target="_blank" rel="noopener noreferrer">GitHub Link</a>
               </div>
             </div>
-          </div>
+          </Element>
         </div>
 
         <div className='sectionThree'>
-          <div id="contact" className="contact" data-aos="zoom-in">
-            <h1 className='text-white'>Contact</h1>
+          <Element name='contact' className="contact" data-aos="zoom-in">
+            <h1 className='text-[var(--txt)]'>Contact</h1>
             <div className="contactContent">
               <p>Phone: </p>
               <p>Email: </p>
               <p>GitHub: <a className='text-[rgb(255,0,0)] decoration-transparent' href="https://github.com/DamianAr420" target="_blank" rel="noopener noreferrer">GitHub</a></p>
             </div>
-          </div>
+          </Element>
         </div>
 
       </div>}
